512caf90adda44607c0f27be77d6066802978a02
yuval ronen <yuvalo@mail.tau.ac.il>
2021-04-15 20:37:55 +0300

    .

--- google/src/App.js
+++ google/src/App.js
@@ -1,3 +1,23 @@
+// import './App.css'
+// import navbar from "./components/navbar"
+// import {BrowserRouter as Router, Switch, Route } from "react-router-dom"
+// import "./App.css"
+//
+// function App() {
+//   return (
+//       <>
+//         <Router>
+//           <navbar />
+//           <Switch>
+//               <Route path = '/' exact />
+//           </Switch>
+//         </Router>
+//       </>
+//   );
+// }
+//
+// export default App;
+
 import logo from './logo.svg';
 import './App.css';
 
