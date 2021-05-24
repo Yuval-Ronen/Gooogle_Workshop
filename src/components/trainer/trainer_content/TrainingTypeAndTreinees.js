e9c9d77c6b0b971f307033acd7d7f5b7edf8aa27
Shahar-G-R <shacharreinis@gmail.com>
2021-05-24 09:59:47 +0300

    Calendar change
    Add event form isn't finish

---
+++ src/components/trainer/trainer_content/TrainingTypeAndTreinees.js
@@ -0,0 +1,64 @@
+import {
+    pink, purple, teal, amber, deepOrange, lightGreen, deepPurple
+  } from '@material-ui/core/colors';
+
+  export const triningType = [
+    {
+      text: 'אישי',
+      id: 1,
+      color: amber,
+    }, {
+      text: 'VIP',
+      id: 2,
+      color: pink,
+    }, {
+      text: 'מתנדב',
+      id: 3,
+      color: purple,
+    }, {
+      text: 'קבוצתי',
+      id: 4,
+      color: deepOrange,
+    }, {
+      text: 'מוסד',
+      id: 5,
+      color: teal,
+    }, {
+        text: 'Online',
+        id: 6,
+        color: lightGreen,
+      }
+  ];
+
+
+  export const Trainees = [
+    {
+      text: 'רותם גביש',
+      id: 1,
+      color: deepPurple[100],
+    }, {
+      text: 'גל גולדשטיין',
+      id: 2,
+      color: deepPurple[200],
+    }, {
+      text: 'אורי מרום',
+      id: 3,
+      color: deepPurple[300],
+    }, {
+      text: 'עומר צוויג',
+      id: 4,
+      color: deepPurple[400],
+    }, {
+      text: 'דור שירן',
+      id: 5,
+      color: deepPurple[500],
+    }, {
+        text: 'רותם מאירצוק',
+        id: 6,
+        color: deepPurple[600],
+      }, {
+        text:  'הגר דהן',
+        id: 7,
+        color: deepPurple[700],
+      }
+  ];
