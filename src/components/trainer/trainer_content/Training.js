898ee033b9a37cb0ed7595da19021f9a1cfecb79
yuval ronen <yuvalo@mail.tau.ac.il>
2021-06-15 22:47:39 +0300

    before milestone

--- src/components/trainer/trainer_content/Training.js
+++ src/components/trainer/trainer_content/Training.js
@@ -1,20 +1,16 @@
 export const appointments = [
-    {title: 'ריצה', triningType: 'ריצה', startDate: new Date(2021, 2, 24, 10, 0), endDate: new Date(2021, 2, 24, 10, 0), id: 9, 'TrainingDetailsId': 2, Trainees: ['שחר גרינברג ריניס'], moreInfo: 'אין תיאור'},
-{title: 'קארטה', triningType: 'קארטה', startDate: new Date(2021, 3, 23, 8, 0), endDate: new Date(2021, 3, 23, 8, 0), id: 8, 'TrainingDetailsId': 1, Trainees: ['מיטל ברגר', ' ', 'אורי בירנבוים', ' ', 'שחר גרינברג ריניס'], moreInfo: 'אין תיאור'},
-{title: 'ריקוד', triningType: 'ריקוד', startDate: new Date(2021, 4, 13, 11, 0), endDate: new Date(2021, 4, 13, 11, 0), id: 4, 'TrainingDetailsId': 1, Trainees: ['הודאל כהן'], moreInfo: 'להגיע בזמן'},
-{title: 'ריקוד', triningType: 'ריקוד', startDate: new Date(2021, 4, 20, 12, 0), endDat: new Date(2021, 4, 20, 12, 0), id: 11, 'TrainingDetailsId': 1, Trainees: ['הודאל כהן', ' ', 'ירון פרידמן', ' ', 'שחר גרינברג ריניס'], moreInfo: 'אין תיאור'},
-{title: 'קארטה', triningType: 'קארטה', startDate: new Date(2021, 5, 5, 13, 0), endDate: new Date(2021, 5, 5, 13, 0), id: 3, 'TrainingDetailsId': 1, Trainees: ['יובל רונן'], moreInfo: 'אין תיאור'},
-{title: 'קרוספיט', triningType: 'קרוספיט', startDate: new Date(2021, 5, 31, 12, 0), endDate: new Date(2021, 5, 31, 12, 0), id: 10, 'TrainingDetailsId': 2, Trainees: ['שקד טרנר', ' ', 'שחר גרינברג ריניס'], moreInfo: 'אין תיאור'},
-{title: 'שחייה', triningType: 'שחייה', startDate: new Date(2021, 6, 16, 13, 0), endDate: new Date(2021, 6, 16, 13, 0), id: 1, 'TrainingDetailsId': 1, Trainees: ['שחר גרינברג ריניס'], moreInfo: 'אין תיאור'},
-{title: 'ריקוד', triningType: 'ריקוד', startDate: new Date(2021, 6, 17, 18, 0), endDate: new Date(2021, 6, 17, 18, 0), id: 7, 'TrainingDetailsId': 1, Trainees: ['אורי בירנבוים', ' ', 'שחר גרינברג ריניס'], moreInfo: 'אין תיאור'},
-{title: 'קרוספיט', triningType: 'קרוספיט', startDate: new Date(2021, 6, 17, 13, 0), endDate: new Date(2021, 6, 17, 13, 0), id: 2, 'TrainingDetailsId': 2, Trainees: ['יובל רונן', ' ', 'שחר גרינברג ריניס'], moreInfo: 'אין תיאור'},
-{title: 'שחייה', triningType: 'שחייה', startDate: new Date(2021, 7, 4, 9, 0), endDate: new Date(2021, 7, 4, 9, 0), id: 5, 'TrainingDetailsId': 2, Trainees: ['שחר גרינברג ריניס'], moreInfo: 'לא לשכוח משקפת'},
-{title: 'קארטה', triningType: 'קארטה', startDate: new Date(2021, 7, 12, 11, 0), endDate: new Date(2021, 7, 12, 11, 0), id: 6, 'TrainingDetailsId': 1, Trainees: ['יובל רונן', ' ', 'ירון פרידמן'], moreInfo: 'אין תיאור'}]
-//   {
-//     title: 'קארטה',
-//     triningType: 'קארטה',
-//     startDate: new Date(2021, 4, 25, 9, 30),
-//     endDate: new Date(2021, 4, 25, 10, 30),
+    {title: 'ריצה', triningType: 'ריצה', startDate: new Date(2021, 1, 24, 10, 0), endDate: new Date(2021, 2, 24, 10, 0), id: 9, TrainingDetailsId: 2, Trainees: ['שחר גרינברג ריניס'], moreInfo: 'אין תיאור'},
+{title: 'קארטה', triningType: 'קארטה', startDate: new Date(2021, 2, 23, 8, 0), endDate: new Date(2021, 3, 23, 8, 0), id: 8, TrainingDetailsId: 1, Trainees: ['מיטל ברגר', ' ', 'אורי בירנבוים', ' ', 'שחר גרינברג ריניס'], moreInfo: 'אין תיאור'},
+{title: 'ריקוד', triningType: 'ריקוד', startDate: new Date(2021, 3, 13, 11, 0), endDate: new Date(2021, 4, 13, 11, 0), id: 4, TrainingDetailsId: 1, Trainees: ['הודאל כהן'], moreInfo: 'להגיע בזמן'},
+{title: 'ריקוד', triningType: 'ריקוד', startDate: new Date(2021, 3, 20, 12, 0), endDat: new Date(2021, 4, 20, 12, 0), id: 11, TrainingDetailsId: 1, Trainees: ['הודאל כהן', ' ', 'ירון פרידמן', ' ', 'שחר גרינברג ריניס'], moreInfo: 'אין תיאור'},
+{title: 'קארטה', triningType: 'קארטה', startDate: new Date(2021, 4, 5, 13, 0), endDate: new Date(2021, 5, 5, 13, 0), id: 3, TrainingDetailsId: 1, Trainees: ['יובל רונן'], moreInfo: 'אין תיאור'},
+{title: 'קרוספיט', triningType: 'קרוספיט', startDate: new Date(2021, 4, 31, 12, 0), endDate: new Date(2021, 5, 31, 12, 0), id: 10, TrainingDetailsId: 2, Trainees: ['שקד טרנר', ' ', 'שחר גרינברג ריניס'], moreInfo: 'אין תיאור'},
+{title: 'שחייה', triningType: 'שחייה', startDate: new Date(2021, 5, 16, 13, 0), endDate: new Date(2021, 6, 16, 13, 0), id: 1, TrainingDetailsId: 1, Trainees: ['שחר גרינברג ריניס'], moreInfo: 'אין תיאור'},
+{title: 'ריקוד', triningType: 'ריקוד', startDate: new Date(2021, 5, 17, 18, 0), endDate: new Date(2021, 6, 17, 18, 0), id: 7, TrainingDetailsId: 1, Trainees: ['אורי בירנבוים', ' ', 'שחר גרינברג ריניס'], moreInfo: 'אין תיאור'},
+{title: 'קרוספיט', triningType: 'קרוספיט', startDate: new Date(2021, 5, 17, 13, 0), endDate: new Date(2021, 6, 17, 13, 0), id: 2, TrainingDetailsId: 2, Trainees: ['יובל רונן', ' ', 'שחר גרינברג ריניס'], moreInfo: 'אין תיאור'},
+{title: 'שחייה', triningType: 'שחייה', startDate: new Date(2021, 6, 4, 9, 0), endDate: new Date(2021, 7, 4, 9, 0), id: 5, TrainingDetailsId: 2, Trainees: ['שחר גרינברג ריניס'], moreInfo: 'לא לשכוח משקפת'},
+{title: 'קארטה', triningType: 'קארטה', startDate: new Date(2021, 6, 12, 11, 0), endDate: new Date(2021, 7, 12, 11, 0), id: 6, TrainingDetailsId: 1, Trainees: ['יובל רונן', ' ', 'ירון פרידמן'], moreInfo: 'אין תיאור'}];
+// {title: 'קארטה', triningType: 'קארטה', startDate: new Date(2021, 4, 25, 9, 30), endDate: new Date(2021, 4, 25, 10, 30),
 //     id: 0,
 //     TrainingDetailsId: 1,
 //     Trainees: [1,4],
@@ -112,4 +108,3 @@ export const appointments = [
 //   moreInfo:''
 // },
 // ]
-;
