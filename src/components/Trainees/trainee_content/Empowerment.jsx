3f0f78c4a363ef6a0bbcef5631ab686b26ed7205
yuval ronen <yuvalo@mail.tau.ac.il>
2021-07-26 11:49:12 +0300

    cant edit from trainee + calendar almost fixed

--- src/components/Trainees/trainee_content/Empowerment.jsx
+++ src/components/Trainees/trainee_content/Empowerment.jsx
@@ -3,27 +3,31 @@ import ShowGoogleDocs from "./ShowGoogleDocs";
 import React from "react";
 import {useLocalStorage} from "../../../UtillHook";
 import serverConnector from "../../../server-connector";
+import PictureAsPdfIcon from '@material-ui/icons/PictureAsPdf';
 
 const Empowerment = () => {
     const [userInfo] = useLocalStorage("userInfo",{});
     const [link, setLink] = useState("");
+    const [linkForPdf, setLinkForPdf] = useState("");
+
     useEffect( () => {
 
         serverConnector.getPersonalProgramLink(userInfo.ID).then(res => {
          console.log("link with edit",res);
          let withoutEdit = res.split("/edit");
          console.log("link withoutEdit",withoutEdit[0] +"/preview");
-
+         setLinkForPdf(withoutEdit[0])
          setLink(withoutEdit[0] + "/preview");
      })
      },[])
     return (
-    <div className='empowerment'>
-                    <ShowGoogleDocs source = {link}/>
-
-
-
-    </div>
+        <div>
+            <PictureAsPdfIcon onClick={window.open(linkForPdf + "/export?format=pdf")}>
+            </PictureAsPdfIcon>
+            <div className='empowerment'>
+                            <ShowGoogleDocs source = {link}/>
+            </div>
+        </div>
     )
 
 }
