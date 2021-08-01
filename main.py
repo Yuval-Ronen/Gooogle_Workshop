fb4c96652e58d28960a97d1d326b78276168632f
yuval ronen <yuvalo@mail.tau.ac.il>
2021-08-01 22:04:02 +0300

    req

--- main.py
+++ main.py
@@ -290,6 +290,6 @@ def index():
 
 if __name__ == '__main__':
     # TRAINID=updateExercise({'exDate': '20210728T100000Z', 'train_id': 29})
-    app.run(host='localhost', port="5000", debug=True)
-    # app.run()
+    # app.run(host='localhost', port="5000", debug=True)
+    app.run()
 
