c0e00a8f49f0094a6189c15de3706678695638d9
yuval ronen <yuvalo@mail.tau.ac.il>
2021-08-01 19:58:43 +0300

    First python app

--- main.py
+++ main.py
@@ -289,8 +289,8 @@ def updatePersonalProgramLink(trainee_id, link):
 def react():
     return render_template("index.html")
 
-
-if __name__ == '__main__':
-    # TRAINID=updateExercise({'exDate': '20210728T100000Z', 'train_id': 29})
-    app.run(host='localhost', port="5000", debug=True)
+#
+# if __name__ == '__main__':
+#     # TRAINID=updateExercise({'exDate': '20210728T100000Z', 'train_id': 29})
+#     app.run(host='localhost', port="5000", debug=True)
 
